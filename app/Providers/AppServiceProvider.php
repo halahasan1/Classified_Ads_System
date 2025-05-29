4a19ed9accc7f8d75739147445784f7564275bd1
halahasan1 <hala.hasan.hh399@gmail.com>
2025-05-29 14:46:37 +0300

    add gates $ policies for Ad , Category and Review

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Enums\Permission;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define(Permission::APPROVE_AD->value, fn($user) => $user->isAdmin());
+        Gate::define(Permission::CREATE_CATEGORY->value, fn($user) => $user->isAdmin());
+        Gate::define(Permission::DELETE_REVIEW->value, fn($user, $review) => $user->id === $review->user_id || $user->isAdmin());
     }
 }
